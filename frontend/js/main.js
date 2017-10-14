3172b8fc0c0bc07993a469e04e7ffbf195d57835
Arvind Ramaswami <saramaswami@gmail.com>
2017-10-14 15:01:28 -0300

    modified the fraud-checking file

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,14 +1,14 @@
 function onSubmit() {
     console.log("clicked");
-    name = name.textContent;
-    console.log(name);
-    login.classList.add('hidden');
+};
 
-}
 
-const login = document.querySelector('loginForm')
+const login = document.querySelector('#form');
+console.log(login.textContent);
 let name = document.querySelector('#name');
-const submit = document.querySelector('#finish');
+console.log(name.textContent);
+const submit = document.querySelector('#button');
+console.log(submit.textContent);
 submit.addEventListener('onclick', onSubmit());
 const info = document.querySelector('#info');
  
